6483615a2c308c17031c71b43aaaa3ba689cdb1b
Kir2023 <132088682+Kir2023@users.noreply.github.com>
2023-12-21 17:32:30 +0200

    Done

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # goit-node-hw-01
+
+SCREENSHOTS
+
+Read contacts
+
+https://imgur.com/FSvCcBA
+
+Find contact dy id
+
+https://imgur.com/Y5VmzJr
+
+Add contact
+
+https://imgur.com/mdA8zSq
+
+Remove contact
+
+https://imgur.com/8MG2pSM
---
+++ contacts.js
@@ -0,0 +1,45 @@
+const fs = require("node:fs/promises");
+const path = require("node:path");
+const crypto = require("node:crypto");
+
+const contactsPath = path.join(__dirname, "db", "contacts.json");
+
+async function listContacts() {
+  const data = await fs.readFile(contactsPath);
+  return JSON.parse(data);
+}
+
+async function getContactById(id) {
+  const contacts = await listContacts();
+  const contact = contacts.find((contact) => contact.id === id);
+  return contact || null;
+}
+
+async function removeContact(id) {
+  const contacts = await listContacts();
+  const index = contacts.findIndex((item) => item.id === id);
+  if (index === -1) {
+    return null;
+  }
+  const [result] = contacts.splice(index, 1);
+  fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return result;
+}
+
+async function addContact(data) {
+  const contacts = await listContacts();
+  const newContact = {
+    id: crypto.randomUUID(),
+    ...data,
+  };
+  contacts.push(newContact);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return newContact;
+}
+
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+};
